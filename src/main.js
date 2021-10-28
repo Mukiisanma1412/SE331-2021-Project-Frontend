6cf2a1e85517ac7c3ea074a2c7c3a00c3baf7c21
Mukiisanma1412 <86796241+Mukiisanma1412@users.noreply.github.com>
2021-10-28 16:27:56 +0700

    login page

--- src/main.js
+++ src/main.js
@@ -1,10 +1,35 @@
-import { createApp, reactive } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "nprogress/nprogress.css";
-const GStore = reactive({ flashMessage: "" });
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import GStore from './store'
+import 'nprogress/nprogress.css'
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { FontAwesomeIcon } from './plugins/font-awesome'
 
-createApp(App).use(store).use(router).provide("GStore", GStore).mount("#app");
+const requireComponent = require.context(
+  './components',
+  false,
+  /Base[A-Z]\w+\.(vue|js)$/
+)
+
+// Create a reactive object
+
+const app = createApp(App)
+requireComponent.keys().forEach((fileName) => {
+  const componentConfig = requireComponent(fileName)
+
+  const componentName = upperFirst(
+    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
+  )
+
+  app.component(componentName, componentConfig.default || componentConfig)
+})
+
+app
+  .use(router)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .provide('GStore', GStore)
+  .mount('#app')
+
